2f80ca0a70b12fbe9874196b1b0b78746983eb22
Bhavith Manapoty <catchbhav2002@gmail.com>
2022-11-29 17:01:50 -0500

    base server stuff?

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
-save
+#!/usr/bin/env node
 
+import {roll} from "./lib/roll.js";
+import express from 'express';
+import minimist from 'minimist';
+
+const expr = express();
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
