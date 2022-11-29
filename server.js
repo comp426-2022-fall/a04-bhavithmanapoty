5e589092dfecc744c0cc5f69d1926e9e2cfa9319
Bhavith Manapoty <catchbhav2002@gmail.com>
2022-11-29 17:09:23 -0500

    added base case

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ import minimist from 'minimist';
 const expr = express();
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
+
+
+app.get('*', function (req, res) {
+res.send('404 NOT FOUND');
+});
+	
+app.post('/app/roll/', function(req, res) {
+res.send(roll(req.body.sides, req.body.dice, req.body.rolls));
+});
+	
+app.listen(port);
