757086b02eb6393410eee3d60104e99aa88333f8
Bhavith Manapoty <catchbhav2002@gmail.com>
2022-11-29 17:19:44 -0500

    added server code

--- server.js
+++ server.js
@@ -11,6 +11,34 @@ const port = args.port || 5000;
 expr.use(express.json());
 expr.use(express.urlencoded({extended: true}));
 
+expr.get('/app/', function (req, res) {
+res.send('200 OK');
+});
+
+expr.get('/app/roll/', function (req, res) {
+res.send(roll(6, 2, 1));
+});
+
+expr.get('/app/roll/*/', function (req, res) {
+let url = req.url;
+let array = url.split("/");
+res.send(roll(array[3], 2, 1));
+});
+
+expr.get('/app/roll/*/*/', function (req, res) {
+let url = req.url;
+let array = url.split("/");
+res.send(roll(array[3], array[4], 1));
+});
+
+expr.get('/app/roll/*/*/*/', function (req, res) {
+let url = req.url;
+let array = url.split("/");
+res.send(roll(array[3], array[4], array[5]));
+});
+
+
+
 expr.get('*', function (req, res) {
 res.send('404 NOT FOUND');
 });
